b9b3245060f7d31699365033925393cef86ace1e
Norbert Balaz <n.balaz@outlook.com>
2024-12-15 18:17:21 +0100

    style(frontend): runs prettier

--- frontend/src/context/LogContext.js
+++ frontend/src/context/LogContext.js
@@ -3,24 +3,23 @@ import React, { createContext, useState } from 'react'
 const LogContext = createContext()
 
 export function LogProvider({ children }) {
-    const [logs, setLogs] = useState([])
+  const [logs, setLogs] = useState([])
 
-    const addLog = (newLog) => {
-        const timestamp = new Date().toLocaleTimeString([], { 
-            hour: '2-digit', 
-            minute: '2-digit', 
-            second: '2-digit',
-            hour12: false
-        })
-        
-        setLogs((prevLogs) => [...prevLogs, { message: newLog.status, timestamp: timestamp, type: newLog.type }])
-    }  
+  const addLog = (newLog) => {
+    const timestamp = new Date().toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+    })
 
-    return (
-        <LogContext.Provider value={{ logs, addLog }}>
-            {children}
-        </LogContext.Provider>
-    )
+    setLogs((prevLogs) => [
+      ...prevLogs,
+      { message: newLog.status, timestamp: timestamp, type: newLog.type },
+    ])
+  }
+
+  return <LogContext.Provider value={{ logs, addLog }}>{children}</LogContext.Provider>
 }
 
 export default LogContext
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,14 @@
-import Axios from "axios"
+import Axios from 'axios'
 
-const hostname = typeof window !== "undefined" && window.location.hostname ? window.location.hostname : ""
+const hostname =
+  typeof window !== 'undefined' && window.location.hostname ? window.location.hostname : ''
 
 const api = Axios.create({
-    baseURL: `http://${hostname}:5000/`,
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-    },
+  baseURL: `http://${hostname}:5000/`,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 })
 
 export default api
